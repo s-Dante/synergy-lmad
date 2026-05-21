68ae80b61a0452ae29eefc11efb443380f37f1f3
TomsGhost <mercy40@hotmail.es>
2026-05-20 23:36:33 -0600

    update

---
+++ resources/views/layouts/appwelcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Synergy - @yield('title', 'Inicio')</title>
+
+    @vite(['resources/css/global.css', 'resources/css/layouts/app.css'])
+
+    @stack('styles')
+</head>
+
+<body>
+
+    <!--
+    <x-navbar />
+-->
+
+    <main>
+
+        <!--
+        @yield('content')
+-->
+    </main>
+
+    @stack('scripts')
+</body>
+
+</html>
